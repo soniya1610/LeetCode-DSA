77cc9bd67499fc80031bd842793f979db215ff5a
Soniya Meena <meenasonia243@gmail.com>
2026-06-13 14:38:09 +0530

    Added leetcode 3838 POTD

---
+++ String/wordMapping.java
@@ -0,0 +1,40 @@
+package String;
+
+import java.util.Scanner;
+
+class  wordMapping{
+    public String mapWordWeights(String[] words, int[] weights) {
+        StringBuilder ans = new StringBuilder();
+
+        for (String word : words) {
+            int sum = 0;
+
+            for (char ch : word.toCharArray()) {
+                sum = (sum + weights[ch - 'a']) % 26;
+            }
+
+            char mapped = (char) ('a' + (25 - sum));
+            ans.append(mapped);
+        }
+
+        return ans.toString();
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();  
+        String[] words = new String[N];
+        for (int i = 0; i < N; i++) {
+            words[i] = sc.next();  // Remaining input is the array of integers
+        }
+        int[] weights = new int[26];
+        for (int i = 0; i < 26; i++) {
+            weights[i] = sc.nextInt();
+        }
+        
+        // Call user logic function and print the output
+        wordMapping obj = new wordMapping();
+        String result = obj.mapWordWeights(words, weights);
+        System.out.println(result);
+    }
+      
+}
