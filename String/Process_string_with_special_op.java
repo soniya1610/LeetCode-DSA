2d3638808466ac8a233f628149ddfd8cc2c929d0
Soniya Meena <meenasonia243@gmail.com>
2026-06-17 22:21:32 +0530

    Added LeeCode 3614 POTD solution 17-06-2026

--- String/Process_string_with_special_op.java
+++ String/Process_string_with_special_op.java
@@ -7,16 +7,13 @@ class Solution {
         for (char ch : s.toCharArray()) {
             if (ch >= 'a' && ch <= 'z') {
                 result.append(ch);
-            } 
-            else if (ch == '*') {
+            } else if (ch == '*') {
                 if (result.length() > 0) {
                     result.deleteCharAt(result.length() - 1);
                 }
-            } 
-            else if (ch == '#') {
+            } else if (ch == '#') {
                 result.append(result.toString());
-            } 
-            else if (ch == '%') {
+            } else if (ch == '%') {
                 result.reverse();
             }
         }
